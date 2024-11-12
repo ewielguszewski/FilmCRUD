84c5454b662ead7211031e2c8818c92e27382995
Ernest Wielguszewski <ernest.wielguszewski@microsoft.wsei.edu.pl>
2024-11-12 20:06:54 +0100

    Dodaj pliki projektów.

---
+++ FilmCRUD/Controllers/FilmController.cs
@@ -0,0 +1,79 @@
+﻿using FilmCRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmCRUD.Controllers
+{
+    public class FilmController : Controller
+    {
+        private static IList<Film> films = new List<Film>
+        {
+            new Film { Id = 1, Title = "Film1", Description = "Opis1", Price = 3},
+            new Film { Id = 2, Title = "Film2", Description = "Opis2", Price = 5},
+            new Film { Id = 3, Title = "Film3", Description = "Opis3", Price = 3},
+        };
+
+        // GET: FilmController
+        public ActionResult Index()
+        {
+            return View(films);
+        }
+
+        // GET: FilmController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(films.FirstOrDefault (x => x.Id == id));
+        }
+
+        // GET: FilmController/Create
+        public ActionResult Create()
+        {
+            return View(new Film());
+        }
+
+        // POST: FilmController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Film film)
+        {
+            film.Id = films.Count + 1;
+            films.Add(film);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: FilmController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(films.FirstOrDefault(x => x.Id == id));
+
+        }
+
+        // POST: FilmController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Film film)
+        {
+            var x = films.FirstOrDefault(x => x.Id == id);
+            x.Title = film.Title;
+            x.Description = film.Description;
+            x.Price = film.Price;
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: FilmController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(films.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: FilmController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            Film film = films.FirstOrDefault(x => x.Id == id);
+            films.Remove(film);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
